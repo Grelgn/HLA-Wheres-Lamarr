73d0efb99cde1c8a70458cdc8f69c5155424fe85
Grelgn <grelgn@hotmail.com>
2024-06-26 23:25:57 +0300

    Finish the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+	return (
+		<footer>
+			<a href="https://github.com/Grelgn">
+				<div className="footer-text">
+					<i className="devicon-github-original"></i>
+					<p>DEVELOPED BY GRELGN</p>
+				</div>
+			</a>
+		</footer>
+	);
+}
+
+export default Footer;
