f05af6169888884f6152e9dbcc069e0a239f26c6
Grelgn <grelgn@hotmail.com>
2024-06-26 23:07:40 +0300

    Finish the header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+	return (
+		<header>
+			<img src="./src/assets/HLA_logo.png" alt="" height={75} />
+			<h1>WHERE&apos;S LAMARR?</h1>
+		</header>
+	);
+}
+
+export default Header;
