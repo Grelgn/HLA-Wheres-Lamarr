c05ad623af360f67faf1c1eb902c311c67c08784
Grelgn <grelgn@hotmail.com>
2024-06-29 16:28:17 +0300

    Add start button functionality and start animation

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,45 +1,103 @@
+import { useState, useEffect } from "react";
+
 import Video from "./Video";
 
 import imgCombineLogo from "/src/assets/combine_logo.png";
 import imgBarcode from "/src/assets/barcode.png";
 
 import audHover from "/src/assets/hover.wav";
+import audSelect from "/src/assets/select.wav";
+import audStart from "/src/assets/start.wav";
+import audOpen from "/src/assets/open.wav";
+import audComplete from "/src/assets/complete.wav";
 
 function Game() {
+	const [video, setVideo] = useState("Loop");
+	const [startVisibility, setStartVisibility] = useState(true);
+	const [UIvisibility, setUIvisibility] = useState(true);
 
-  // Button sounds
-  function handleHover() {
-    const audioHover = new Audio(audHover);
-    audioHover.volume = 0.2
-    audioHover.play();
-  }
+	// Sounds
+	const audioSelect = new Audio(audSelect);
+	const audioStart = new Audio(audStart);
+	const audioOpen = new Audio(audOpen);
+	const audioComplete = new Audio(audComplete);
 
-  return (
-    <main>
-      <Video />
-      <div className="game-top">
-        <div className="top-left">
-          <b>ECHO-1-12-7</b>
-        </div>
-        <div className="top-right">
-          <p>BIOTICS VISCON TRAINING STATION 0.3.3.333</p>
-          <img src={imgCombineLogo}/>
-        </div>
-      </div>
-      <div className="game-bottom">
-        <img src={imgBarcode} />
-        <p>CIVIL PROTECTION ANNEX 19-81572</p>
-      </div>
-      <div className="start">
-        <h2>
-          EXPOSE AND TAG ALL PARASITICS
-        </h2>
-        <button onMouseEnter={handleHover}>
-          START TRAINING
-        </button>
-      </div>
-    </main>
-  )
+	audioSelect.volume = 0.4;
+	audioStart.volume = 0.3;
+	audioOpen.volume = 0.3;
+	audioComplete.volume = 0.4;
+
+	// Show-Hide Start button
+	useEffect(() => {
+		const start = document.querySelector(".start");
+		if (startVisibility) {
+			start.classList.remove("invisible");
+		} else {
+			start.classList.add("invisible");
+		}
+	}, [startVisibility]);
+
+	//Show-Hide UI
+	useEffect(() => {
+		const vis = document.querySelector(".game-top");
+		const vis2 = document.querySelector(".game-bottom");
+		if (UIvisibility) {
+			vis.classList.remove("invisible");
+			vis2.classList.remove("invisible");
+		} else {
+			vis.classList.add("invisible");
+			vis2.classList.add("invisible");
+		}
+	}, [UIvisibility]);
+
+	// Button hover
+	function handleHover() {
+		const audioHover = new Audio(audHover);
+		audioHover.volume = 0.4;
+		audioHover.play();
+	}
+
+	// Handle game start
+	function handleStartClick() {
+		audioSelect.play();
+		setVideo("Process");
+		setStartVisibility(false);
+		setUIvisibility(false);
+		audioOpen.play();
+		setTimeout(() => {
+			audioStart.play();
+		}, 1000);
+		setTimeout(() => {
+			audioComplete.play();
+			setVideo("Loop");
+      setUIvisibility(true);
+		}, 9000);
+	}
+
+	return (
+		<main>
+			<Video videoState={video} key={video} />
+			<div className="game-top">
+				<div className="top-left">
+					<b>ECHO-1-12-7</b>
+				</div>
+				<div className="top-right">
+					<p>PARASITIC VISCON TRAINING STATION 0.3.3.333</p>
+					<img src={imgCombineLogo} />
+				</div>
+			</div>
+			<div className="game-bottom">
+				<img src={imgBarcode} />
+				<p>CIVIL PROTECTION ANNEX 19-81572</p>
+			</div>
+			<div className="start">
+				<h2>EXPOSE AND TAG ALL PARASITICS</h2>
+				<button onClick={handleStartClick} onMouseEnter={handleHover}>
+					START TRAINING
+				</button>
+			</div>
+		</main>
+	);
 }
 
 export default Game;
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,12 +1,26 @@
-import vidLoop from "/src/assets/Wupgrade_Loop.mp4";
-import tempBG from "/src/assets/temp_background.png"
+import PropTypes from "prop-types";
 
-function Video() {
+import vidLoop from "/src/assets/Wupgrade_Loop.mp4";
+import vidProcess from "/src/assets/Wupgrade_Process.mp4";
+import tempBG from "/src/assets/temp_background.png";
+
+function Video(props) {
+	let vidSource = "";
+	if (props.videoState == "Loop") vidSource = vidLoop;
+	if (props.videoState == "Process") vidSource = vidProcess;
+
 	return (
 		<video autoPlay muted loop poster={tempBG}>
-			<source src={vidLoop} type="video/mp4" />
+			<source src={vidSource} type="video/mp4" />
 		</video>
 	);
 }
+
+Video.defaultProps = {
+	videoState: "Loop",
+};
+Video.propTypes = {
+	videoState: PropTypes.string,
+};
 
 export default Video;
